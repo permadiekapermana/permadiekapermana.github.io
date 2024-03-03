c3aa4c1c00ab74bcd6912993b7607dbbf53583ac
Permadi Eka Permana <permadiekapermana@gmail.com>
2024-03-03 10:21:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+portofolio-project-v2
---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function openPDF(pdfLink) {
+  window.open(pdfLink, '_blank');
+}
